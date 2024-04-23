655852800ac8afd9d8d3cdd382ae667d52c3fe46
Dan Jenkins <dan@nimblea.pe>
2024-04-23 14:14:13 +0100

    add posthog

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { vitePostHog } from "vite-plugin-posthog";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+
+  return defineConfig({
+    plugins: [
+      react(),
+      vitePostHog({
+        apiKey: "phc_mziibLwvn4pTYWYwf9W8QpSvtEOvWVZE2qKkadiLpJS",
+        hostUrl: "https://eu.posthog.com",
+        isDevModeOn: true,
+        config: {
+          autocapture: false,
+          capture_pageview: false,
+          // ...other posthog config options
+        },
+      }),
+    ],
+  });
+};
