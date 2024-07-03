1c44563eba7c4150e6960af7a620e316bd6b3235
Dan Jenkins <dan@nimblea.pe>
2024-07-03 10:28:51 +0100

    add a function to get provider results

--- functions/api/[provider].js
+++ functions/api/[provider].js
@@ -1,6 +1,7 @@
 import { calculateBestAndWorst, refactorData } from './util';
 
 export async function onRequest(context) {
+
 	// Initialize the default cache
 	//const cache = caches.default
 
@@ -29,16 +30,10 @@ export async function onRequest(context) {
 	// // before returning the response we put a clone of our response object into the cache so it can be resolved later
 	// event.waitUntil(cache.put(event.request, response.clone()))
 
-	const providerData_kv = await context.env['iceperf-cache'].get('7-day-average');
+	const data = await context.env['iceperf-cache'].get(`7-day-${context.params.provider}-throughput`);
 
-	const refactoredData = refactorData(JSON.parse(providerData_kv));
 
-	const response = new Response(JSON.stringify({
-		bestAndWorstProvider: calculateBestAndWorst(refactoredData.commercial),
-		bestAndWorstOSS: calculateBestAndWorst(refactoredData.oss),
-		providerData: refactoredData.commercial,
-		ossData: refactoredData.oss,
-	}), {
+	const response = new Response(JSON.stringify(data), {
 		headers: {
 			// We set a max-age of 300 seconds which is equivalent to 5 minutes.
 			// If the last response is older than that the cache.match() call returns nothing and and a new response is fetched
