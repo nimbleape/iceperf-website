91a7319cbfe35cd9b1a02e896e9a9a1f38ad7539
marcovidonis <marco@nimblea.pe>
2024-07-04 17:12:35 +0100

    update About copy and add icons

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,11 @@
-import { Layout } from '../layout/Layout'
-import { Typography } from '../components/Typography'
-import { Blocks } from "../components/Blocks"
-import { Card } from "../components/Card"
-import { Foo } from "../icons/Foo"
+import { Layout } from '../layout/Layout';
+import { Typography } from '../components/Typography';
+import { Blocks } from '../components/Blocks';
+import { Card } from '../components/Card';
+import { Foo } from '../icons/Foo';
+import ArrowUpDown from '../icons/ArrowUpDown';
+import Cable from '../icons/Cable';
+import Stopwatch from '../icons/Stopwatch';
 
 export function About() {
   return (
@@ -14,13 +17,39 @@ export function About() {
       <Typography style='body'>
         ICEPerf runs a series of tests with each of the TURN providers mentioned above and exports metrics to compare the results.
       </Typography>
-      <Typography style='body'>Learn more on the <a className='text-ipblue-900 underline' href='https://nimblea.pe/monkey-business/2024/04/30/introducing-iceperf-com/' target='_blank'>Nimble Ape blog post.</a></Typography>
+      <Typography style='body'>
+        Learn more on the <a className='text-ipblue-900 underline' href='https://nimblea.pe/monkey-business/2024/04/30/introducing-iceperf-com/' target='_blank'>Nimble Ape blog post</a>.
+      </Typography>
+
+      <Typography style='h3'>Description of the tests</Typography>
+      <Typography style='body'>
+        Each test is carried out between two clients running on the same machine, an Offerer and an Answerer. While normally an agent will gather all available ICE candidates when establishing a WebRTC connection, ICEPerf specifies the scheme, protocol and port for each test, and then runs all tests in sequence.
+      </Typography>
+      <Typography style='body'>
+        For tests over TURN networks, only the Offerer is forced to go through TURN.
+      </Typography>
 
       <Blocks>
-        <Card title="Latency" content="Time to first byte." icon={Foo} />
-        <Card title="Ice Candidate" content="Time to recieve an Ice Candidate" icon={Foo} />
-        <Card title="Throughput" content="Rate of data send" icon={Foo} />
+        <Card
+          title='Latency'
+          content='Measures the time until the Answerer receives the first packet over a TURN network.'
+          icon={<Stopwatch className='flex-shrink-0 size-6 text-blue-600 dark:text-blue-400' />}
+        />
+        <Card
+          title='ICE Candidate'
+          content='Determines the time taken to receive the first ICE candidate, which is crucial for establishing WebRTC connections quickly and efficiently.'
+          icon={<Cable className='flex-shrink-0 size-6 text-blue-600 dark:text-blue-400' />}
+        />
+        <Card
+          title='Throughput'
+          content='Once the connection via a TURN server is established, the Offerer agent starts sending 1-MB packets to the Answerer through the data channel, as quickly as possible. The throughput is the calculated rate of packets received by the Answerer.'
+          icon={<ArrowUpDown className='flex-shrink-0 size-6 text-blue-600 dark:text-blue-400' />}
+        />
       </Blocks>
+
+      <Typography style='small'>
+        NOTE: Tests are completed over a home broadband connection, 1 Gbps down, 100 Mbps up, hardwired. Tests are run every hour. Average results shown on this website are 7-day averages, updated hourly. Throughput trends show the latest available measurement.
+      </Typography>
     </Layout>
   )
 }
---
+++ src/icons/ArrowUpDown.jsx
@@ -0,0 +1,32 @@
+import { twMerge } from 'tailwind-merge';
+import PropTypes from 'prop-types';
+
+const ArrowUpDown = ({ className }) => (
+  <svg
+    className={twMerge('inline-block size-4 self-center', className)}
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path d='m21 16-4 4-4-4' />
+    <path d='M17 20V4' />
+    <path d='m3 8 4-4 4 4' />
+    <path d='M7 4v16' />
+  </svg>
+);
+
+export default ArrowUpDown;
+
+ArrowUpDown.propTypes = {
+  className: PropTypes.string,
+};
+
+ArrowUpDown.defaultProps = {
+  className: '',
+};
