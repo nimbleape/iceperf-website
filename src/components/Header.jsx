aeb297400c7deebe69fed50e5e46f642d9996900
marcovidonis <marco@nimblea.pe>
2024-10-01 17:42:36 +0100

    improve Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ import { providers, projects } from '../constants';
 import { IcePerfLogo } from './IcePerfLogo';
 
 export function Header() {
-  const { user, isLoading, signIn, signUp, signOut } = useAuth();
+  const { user, isLoading, signIn, signUp } = useAuth();
   const location = useLocation();
 
   return (
@@ -19,14 +19,21 @@ export function Header() {
 
       <nav className='relative max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8' aria-label='Global'>
         <div className='flex items-center justify-between'>
-          <Link className='flex-none text-xl font-semibold dark:text-white' to='/' aria-label='IcePerf'>
+          <Link className='flex-none pt-3 max-w-40 md:max-w-56 lg:max-w-md text-xl font-semibold dark:text-white' to='/' aria-label='IcePerf'>
             <IcePerfLogo kind='side' />
           </Link>
-          <div className='sm:hidden'>
-            <button type='button' className='hs-collapse-toggle size-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700' data-hs-collapse='#navbar-collapse-with-animation' aria-controls='navbar-collapse-with-animation' aria-label='Toggle navigation'>
-              <svg className='hs-collapse-open:hidden flex-shrink-0 size-4' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'><line x1='3' x2='21' y1='6' y2='6'/><line x1='3' x2='21' y1='12' y2='12'/><line x1='3' x2='21' y1='18' y2='18'/></svg>
-              <svg className='hs-collapse-open:block hidden flex-shrink-0 size-4' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'><path d='M18 6 6 18'/><path d='m6 6 12 12'/></svg>
-            </button>
+          <div className='flex items-center justify-between'>
+            <div className='sm:hidden'>
+              <button type='button' className='hs-collapse-toggle size-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700' data-hs-collapse='#navbar-collapse-with-animation' aria-controls='navbar-collapse-with-animation' aria-label='Toggle navigation'>
+                <svg className='hs-collapse-open:hidden flex-shrink-0 size-4' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'><line x1='3' x2='21' y1='6' y2='6'/><line x1='3' x2='21' y1='12' y2='12'/><line x1='3' x2='21' y1='18' y2='18'/></svg>
+                <svg className='hs-collapse-open:block hidden flex-shrink-0 size-4' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'><path d='M18 6 6 18'/><path d='m6 6 12 12'/></svg>
+              </button>
+            </div>
+            {!!user && (
+              <div className='ml-4 sm:hidden'>
+                <NavItem label='Settings' to='/settings' />
+              </div>
+            )}
           </div>
         </div>
         <div id='navbar-collapse-with-animation' className='hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block'>
@@ -45,17 +52,7 @@ export function Header() {
               ))}
             </NavMenu>
 
-            {user ? (
-              <>
-                <NavItem label='Settings' to='/settings' />
-                <Button
-                  onClick={() => signOut()}
-                  disabled={isLoading}
-                >
-                  Sign Out
-                </Button>
-              </>
-            ) : (
+            {!user && (
               <>
                 <Button
                   onClick={() => signIn({ context: location.search, state: { returnTo: location.pathname } })}
@@ -73,6 +70,11 @@ export function Header() {
             )}
           </div>
         </div>
+        {!!user && (
+          <div className='ml-4 hidden sm:block'>
+            <NavItem label='Settings' to='/settings' />
+          </div>
+        )}
       </nav>
     </header>
   )
