93edfc8d2b7f6e0d34abad1d755ae4dd7c6f01ab
marcovidonis <marco@nimblea.pe>
2024-10-01 16:49:48 +0100

    make Typography customisable

--- src/components/Typography.jsx
+++ src/components/Typography.jsx
@@ -1,17 +1,55 @@
-// eslint-disable-next-line react/prop-types
-export const Typography = ({ style, children }) => {
+import PropTypes from 'prop-types';
+import { twMerge } from 'tailwind-merge';
+
+export const Typography = ({ style, children, className }) => {
   switch (style) {
     case 'body':
-      return <p className='mt-3 text-lg text-gray-800 dark:text-neutral-400'>{children}</p>;
+      return <p className={twMerge('mt-3 text-lg text-gray-800 dark:text-neutral-400', className)}>{children}</p>;
     case 'h1':
-      return <h1 className='block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-6xl lg:leading-tight dark:text-white'>{children}</h1>;
+      return (
+        <h1
+          className={twMerge('block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-6xl lg:leading-tight dark:text-white', className)}
+        >
+          {children}
+        </h1>
+      );
     case 'h2':
-      return <h2 className='my-8 text-3xl text-gray-800 font-bold lg:text-4xl dark:text-white'>{children}</h2>;
+      return (
+        <h2
+          className={twMerge('my-8 text-3xl text-gray-800 font-bold lg:text-4xl dark:text-white', className)}
+        >
+          {children}
+        </h2>
+      );
     case 'h3':
-      return <h3 className='mb-2 mt-6 text-2xl text-gray-800 font-semibold lg:text-3xl dark:text-white'>{children}</h3>;
+      return (
+        <h3
+          className={twMerge('mb-2 mt-6 text-2xl text-gray-800 font-semibold lg:text-3xl dark:text-white', className)}
+        >
+          {children}
+        </h3>
+      );
+    case 'h4':
+      return (
+        <h4
+          className={twMerge('mb-2 mt-6 text-xl text-gray-800 font-semibold lg:text-2xl dark:text-white', className)}
+        >
+          {children}
+        </h4>
+      );
     case 'small':
-      return <p className='mt-3 text-gray-800 dark:text-neutral-400'><small>{children}</small></p>;
+      return (
+        <p className={twMerge('mt-3 text-gray-800 dark:text-neutral-400', className)}>
+          <small>{children}</small>
+        </p>
+      );
     default:
-      return <p>{children}</p>;
+      return <p className={className}>{children}</p>;
   }
+};
+
+Typography.propTypes = {
+  style: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.func,
 };
