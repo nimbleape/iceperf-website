5f87fb80d9f4736d0b15a3013d6cb2a76bdbf3f4
marcovidonis <marco@nimblea.pe>
2024-10-01 17:58:19 +0100

    fix ButtonLink component

--- src/components/ButtonLink.jsx
+++ src/components/ButtonLink.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-export function Button({ to = '', label = '', icon = null }) {
+export function ButtonLink({ to = '', label = '', icon = null }) {
   return (
     <Link
       className='py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-ipblue-900 text-white hover:bg-ipblue-800 disabled:opacity-50 disabled:pointer-events-none'
@@ -13,7 +13,7 @@ export function Button({ to = '', label = '', icon = null }) {
   )
 }
 
-Button.propTypes = {
+ButtonLink.propTypes = {
   label: PropTypes.string,
   to: PropTypes.string,
   icon: PropTypes.node
