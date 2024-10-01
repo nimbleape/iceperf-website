7ffa7694c5622bd65de0071327b4b8425a426998
marcovidonis <marco@nimblea.pe>
2024-10-01 17:46:08 +0100

    move HamburgerButton in its own component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { Link, useLocation } from 'react-router-dom';
 import { useAuth } from '@workos-inc/authkit-react';
 
 import { Button } from '../components/Button';
+import { HamburgerButton } from '../components/HamburgerButton';
 import { NavMenuItem } from './NavMenuItem';
 import { NavMenu } from './NavMenu';
 import { NavItem } from './NavItem';
@@ -24,10 +25,7 @@ export function Header() {
           </Link>
           <div className='flex items-center justify-between'>
             <div className='sm:hidden'>
-              <button type='button' className='hs-collapse-toggle size-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700' data-hs-collapse='#navbar-collapse-with-animation' aria-controls='navbar-collapse-with-animation' aria-label='Toggle navigation'>
-                <svg className='hs-collapse-open:hidden flex-shrink-0 size-4' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'><line x1='3' x2='21' y1='6' y2='6'/><line x1='3' x2='21' y1='12' y2='12'/><line x1='3' x2='21' y1='18' y2='18'/></svg>
-                <svg className='hs-collapse-open:block hidden flex-shrink-0 size-4' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'><path d='M18 6 6 18'/><path d='m6 6 12 12'/></svg>
-              </button>
+              <HamburgerButton />
             </div>
             {!!user && (
               <div className='ml-4 sm:hidden'>
