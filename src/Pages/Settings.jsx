9b6f0395e7489c50a34cecd194984aeec1352b45
marcovidonis <marco@nimblea.pe>
2024-10-02 17:50:17 +0100

    add link to Customer Portal on Settings page

--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -1,6 +1,7 @@
 import { useAuth } from '@workos-inc/authkit-react';
 
 import { Button } from '../components/Button';
+import { ButtonLink } from '../components/ButtonLink';
 import { Layout } from '../layout/Layout';
 import { Typography } from '../components/Typography';
 
@@ -11,6 +12,11 @@ export function Settings() {
     <Layout>
       <Typography style='h2' className='mb-0'>Account Settings</Typography>
       <Typography style='h4' className='text-gray-500 mt-1 mb-8'>{user?.firstName} {user?.lastName}</Typography>
+      <ButtonLink
+        className='text-ipblue-200 bg-ipblue-800'
+        label='Customer Portal'
+        to='https://billing.stripe.com/p/login/test_eVa2accDp7pj9c4288'
+      />
       <Button
         className='text-red-800'
         onClick={() => signOut()}
